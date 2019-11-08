7bfe5a4e41e826be66dbe199ef27f8dae7a31774
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-08 15:02:44 -0500

    import utility fns

--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -5,104 +5,17 @@ import {
   CardTitle , CardSubtitle , CardText
 } from "reactstrap";
 
+/// internal modules ///
+import {
+  getPronoun,
+  conjugate as conj,
+} from "../utils/persons";
+import {
+  capitalize as cap,
+} from "../utils/strings";
+
 /// styles ///
 import "./StarWars.css";
-
-/***************************************
-  HELPERS
-***************************************/
-const getPronoun = (gender) => {
-  switch (gender.toLowerCase ()) {
-    case "female":
-    case "f":
-      return ("she");
-    case "male":
-    case "m":
-      return ("he");
-    default:
-      return ("they");
-  }
-}
-
-const capitalize = (s) => {
-  return (s.charAt (0).toUpperCase + s.slice (1));
-}
-
-const conj = (pro , verb) => {
-  switch (verb) {
-    case "is":
-      switch (pro) {
-        case "i":
-          return ("am");
-        case "you":
-          return ("are");
-        case "she":
-        case "he":
-        case "it":
-          return ("is");
-        case "we":
-          return ("are");
-        case "yous":
-        case "you all":
-        case "y'all":
-          return ("are");
-        case "they":
-          return ("are");
-        default:
-          return (verb);
-      }
-    case "was":
-      switch (pro) {
-        case "i":
-          return ("was");
-        case "you":
-          return ("were");
-        case "she":
-        case "he":
-        case "it":
-          return ("was");
-        case "we":
-          return ("were");
-        case "yous":
-        case "you all":
-        case "y'all":
-          return ("were");
-        case "they":
-          return ("were");
-        default:
-          return (verb);
-      }
-    case "will be":
-      return (verb);
-    case "has":
-      switch (pro) {
-        case "i":
-          return ("have");
-        case "you":
-          return ("have");
-        case "she":
-        case "he":
-        case "it":
-          return ("has");
-        case "we":
-          return ("have");
-        case "yous":
-        case "you all":
-        case "y'all":
-          return ("have");
-        case "they":
-          return ("have");
-        default:
-          return (verb);
-      }
-    case "had":
-      return (verb);
-    case "will have":
-      return (verb);
-    default:
-      return (verb);
-  }
-}
 
 /***************************************
   COMPONENTS
@@ -116,8 +29,8 @@ const PeopleCard = ({data}) => {
         <CardTitle>{data.name}</CardTitle>
         <CardText>
           {data.name} was born in {data.birth_year}.
-          {capitalize (pro)} {conj (pro , "has")} {data.hair_color.toLowerCase ()} hair, {data.eye_color.toLowerCase ()} eyes, and {data.skin_color.toLowerCase ()} skin.
-          {capitalize (pro)} {conj (pro , "is")} {data.mass}kg and {data.height}cm tall.
+          {cap (pro)} {conj (pro , "has")} {data.hair_color.toLowerCase ()} hair, {data.eye_color.toLowerCase ()} eyes, and {data.skin_color.toLowerCase ()} skin.
+          {cap (pro)} {conj (pro , "is")} {data.mass}kg and {data.height}cm tall.
         </CardText>
       </CardBody>
     </Card>
