de74150d0540674b27954a7678aa64dab76653a0
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-08 12:51:08 -0500

    add code to fetch people data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -12,8 +12,11 @@ import "./App.css";
   STATES
 ***************************************/
 const init = {
-  "people" : [],
+  "people" : {},
+  "query" : {}, // if ever wanted to specify a person's id or something else
 };
+
+const fetchPeople = fetchData (peopleAPI , init.people);
 
 /***************************************
   COMPONENTS
@@ -21,10 +24,15 @@ const init = {
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [people , setPeople] = React.useState (init.people);
+  const [query , setQuery] = React.useState (init.query);
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  React.useEffect (() => {
+    fetchPeople (query , setPeople);
+  } , [query]);
 
   return (
     <div className="App">
