63ee021ce11c023bebfc369fcafce3bdc02abc11
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-08 13:14:37 -0500

    create components to fill out

---
+++ starwars/src/components/PeopleCard.js
@@ -0,0 +1,17 @@
+/// external modules ///
+import React from "react";
+
+/// styles ///
+import "./StarWars.css";
+
+/***************************************
+  COMPONENTS
+***************************************/
+const PeopleCard = (props) => {
+  return (
+    <></>
+  );
+};
+
+/**************************************/
+export default PeopleCard;
---
+++ starwars/src/components/PeopleGrid.js
@@ -0,0 +1,20 @@
+/// external modules ///
+import React from "react";
+
+/// internal modules ///
+import PeopleCard from "./PeopleCard";
+
+/// styles ///
+import "./StarWars.css";
+
+/***************************************
+  COMPONENTS
+***************************************/
+const PeopleGrid = (props) => {
+  return (
+    <></>
+  );
+};
+
+/**************************************/
+export default PeopleGrid;
